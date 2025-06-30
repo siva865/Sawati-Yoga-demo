f888458f03fa1543bc37fb1c4629d5fe430ffb18
sabapathi <sabapathisiranjeevi@gmail.com>
2025-06-30 10:05:36 +0530

    Initial commit of my Vite project

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ViteFonts from 'vite-plugin-fonts'
+import react from '@vite/plugin-react'
+
 
 export default defineConfig({
   plugins: [react()],
